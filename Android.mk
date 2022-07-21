dff2a534c783a670d53828287600d9914d1bd52e
me-cafebabe <me_cafebabe@hotmail.com>
2022-07-22 00:28:56 +0800

    sm8250-common: Welcome alioth

--- Android.mk
+++ Android.mk
@@ -26,7 +26,7 @@
 LOCAL_PATH := $(call my-dir)
 
 MILITO_DEVICES := monet vangogh
-MIKONA_DEVICES := mikona_a umi elish
+MIKONA_DEVICES := mikona_a umi elish alioth
 OTHER_DEVICES := mobius
 
 ifeq ($(TARGET_DEVICE),$(filter $(TARGET_DEVICE),$(MILITO_DEVICES) $(MIKONA_DEVICES) $(OTHER_DEVICES)))
